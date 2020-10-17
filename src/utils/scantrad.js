8aa31a3b9896c0d1eb97761dfac8ff233659197e
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2020-10-17 12:21:12 +0000

    for now it can : - add new manga automatically - update manga chapterData with fresh new one

--- src/utils/scantrad.js
+++ src/utils/scantrad.js
@@ -1,7 +1,6 @@
 const Mangas = require("../db/model/manga"),
-    generateMessage = require("./messageFormatter"),
-    notify = require("./notifier");
-
+    { areTheSame, generateMessage } = require("./quickUtility");
+require("../db/mongoose");
 const osmosis = require("osmosis"),
     lastPublished = require("./scrapper");
 let chapter = [];
@@ -12,36 +11,100 @@ let i = 0;
 //     console.log(i, chapter);
 // });
 
+// setInterval(() => {
+//     (async () => {
+//         //get the last published chapter on scantrad
+//         const chapter = await lastPublished();
+//         if (chapter && chapter != {}) {
+//             console.log(chapter);
+//             const lastStored = await Mangas.findOne({
+//                 mangaName: chapter.manga,
+//             });
+//             console.log(lastStored);
+//             if (lastStored) {
+//                 //check is lastStored == current chapter
+//                 if (chapter.num != lastStored.chapterData.num) {
+//                     //update
+//                     const updated = await Mangas.updateOne(
+//                         { mangaName: chapter.manga },
+//                         {
+//                             chapterData: {
+//                                 title: chapter.title,
+//                                 link: "https://scantrad.fr/" + chapter.link,
+//                                 num: chapter.num,
+//                             },
+//                         }
+//                     );
+//                     console.log(updated);
+//                 } else {
+//                     //pass
+//                     console.log("Passed");
+//                 }
+//             } else {
+//                 //Most likely a new project they picked up, so we add.
+//                 try {
+//                     const stored = await Mangas.create({
+//                         mangaName: chapter.manga,
+//                         chapterData: {
+//                             title: chapter.title,
+//                             link: "https://scantrad.fr/" + chapter.link,
+//                             num: chapter.num,
+//                         },
+//                     });
+//                 } catch (err) {
+//                     //OHH NOOOO!!!!... Anyway.
+//                 }
+//             }
+//         }
+//     })();
+// }, 5000);
 setInterval(() => {
     (async () => {
-        //get the last published chapter on scantrad
+        //Retrieve last published chapter
         const chapter = await lastPublished();
+        console.log("last chapter----- ", chapter);
+
         if (chapter && chapter != {}) {
-            const lastStored = await Mangas.findOne({
-                mangaName: chapter.manga,
-            });
+            let lastStored;
+            //Making sure chapter ain't empty, happened before...
+            try {
+                //try to find retrieved manga in db, return null is not
+                lastStored = await Mangas.findOne({
+                    mangaName: chapter.manga,
+                });
+            } catch (error) {
+                console.log(error.message);
+            }
             if (lastStored) {
-                //check is lastStored == current chapter
-                if (chapter.num != lastStored.chapterData.num) {
-                    //update
-                    const updated = await Mangas.updateOne(
-                        { mangaName: chapter.manga },
-                        {
-                            chapterData: {
-                                title: chapter.title,
-                                link: "https://scantrad.fr/" + chapter.link,
-                                num: chapter.num,
+                //Meaning the manga (by name) is already in db
+                //it can either mean
+                //What's retrived == what's stored or not
+                if (!areTheSame(chapter, lastStored.chapterData)) {
+                    //It's a different chapter
+                    //we update the data then
+                    let updated;
+                    try {
+                        updated = await Mangas.updateOne(
+                            {
+                                mangaName: chapter.manga,
                             },
-                        }
-                    );
-                    console.log("updated");
-                    notify(process.env.GROUP_ID, updated);
+                            {
+                                chapterData: {
+                                    title: chapter.title,
+                                    link: "https://scantrad.fr/" + chapter.link,
+                                    num: chapter.num,
+                                },
+                            }
+                        );
+                    } catch (error) {
+                        console.log(error.message);
+                    }
                 } else {
-                    //pass
-                    console.log("Passed");
+                    console.log("same chapter");
                 }
             } else {
-                //Most likely a new project they picked up, so we add.
+                //lastStored == null, meaning it does not exist in db
+                //It probably is a new manga, so we have to add it to db.
                 try {
                     const stored = await Mangas.create({
                         mangaName: chapter.manga,
@@ -51,11 +114,10 @@ setInterval(() => {
                             num: chapter.num,
                         },
                     });
-                    notify(process.env.GROUP_ID, updated);
-                } catch (err) {
-                    //OHH NOOOO!!!!... Anyway.
+                } catch (error) {
+                    console.log(error.message);
                 }
             }
         }
     })();
-}, 15000);
+}, 5000);
