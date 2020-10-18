6ba70b1e67aefa6fe8dd1cd0db63a957ea9a34e5
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2020-10-18 11:26:40 +0000

    script to create initialize empty blacklist upon first launch

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose"),
-    Mangas = require("./model/manga");
-
+    Mangas = require("./model/manga"),
+    Blacklist = require("./model/blacklist");
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
 });
+
+//Create a new blacklist document if it haven't done so yet.
+Blacklist.findOne()
+    .then((res) => {
+        if (res == null) {
+            Blacklist.create({ blacklisted: [] })
+                .then((res) => {
+                    console.log(res);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
