73f1f199a92e74328afa6ce8403c206a0b1ae5ff
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2020-11-22 18:36:12 +0000

    methods for new commands

--- src/utils/scrapper.js
+++ src/utils/scrapper.js
@@ -35,13 +35,16 @@ const getLastPublished = () => {
             .data((item) => {
                 chapterList.push(item);
             })
-            .error((err) => reject(err))
+            .error((err) => {
+                reject(err);
+            })
             .done(() => resolve(chapterList[0]));
     });
 };
 
 const getMangaState = (manga) => {
-    let out = {};
+    let out = "";
+    let c = 0;
     return new Promise((resolve, reject) => {
         osmosis
             .get("https://scantrad.net/planning")
@@ -51,11 +54,110 @@ const getMangaState = (manga) => {
                 state: ".hma-etat",
             })
             .data((item) => {
-                out[item.manga] = item.state;
+                if (item.manga.startsWith(manga)) {
+                    out = item.manga;
+                    c += 1;
+                }
             })
             .error((err) => reject(err))
-            .done(() => resolve(out[manga]));
+            .done(() => {
+                if (c > 1) {
+                    reject(
+                        `There is ${c} mangas matching this title, please be more specific.`
+                    );
+                }
+
+                osmosis
+                    .get("https://scantrad.net/planning")
+                    .find(".home-manga")
+                    .set({
+                        manga: "@href",
+                        state: ".hma-etat",
+                    })
+                    .data((item) => {
+                        if (item.manga.startsWith(manga)) {
+                            out = item.state;
+                        }
+                    })
+                    .error((err) => reject(err))
+                    .done(() => resolve(out));
+            });
     });
 };
 
-module.exports = { lastPublished, getLastPublished, getMangaState };
+const getScantradMangas = () => {
+    let out = [];
+    return new Promise((resolve, reject) => {
+        osmosis
+            .get("https://scantrad.net/planning")
+            .find(".home-manga")
+            .set({
+                manga: "@href",
+                state: ".hma-etat",
+            })
+            .data((item) => {
+                item = item.manga.replace(/-/g, " ");
+                out.push(item);
+            })
+            .error((err) => reject(err))
+            .done(() => resolve(out));
+    });
+};
+
+const getMangaDetails = (manga) => {
+    let out = "";
+    let c = 0; //Number of match
+    return new Promise((resolve, reject) => {
+        osmosis
+            .get("https://scantrad.net/planning")
+            .find(".home-manga")
+            .set({
+                manga: "@href",
+                state: ".hma-etat",
+            })
+            .data((item) => {
+                if (item.manga.startsWith(manga)) {
+                    out = item.manga;
+                    c += 1;
+                }
+            })
+            .error((err) => reject(err))
+            .done(() => {
+                if (c > 1) {
+                    reject(
+                        `There is ${c} mangas that match with this title, please be more specific.`
+                    );
+                }
+
+                //Is there is only one item that match
+                osmosis
+                    .get(`https://scantrad.net/${out}`)
+                    .delay(500)
+                    .find("div.main-fiche > div.mf-info")
+                    .set({
+                        synopsis: "div.synopsis",
+                    })
+                    .data((data) => {
+                        out = data.synopsis;
+                    })
+                    .error((err) => {
+                        reject(err);
+                    })
+                    .done(() => {
+                        if (out) {
+                            resolve(out);
+                        } else {
+                            reject("Unexpected error");
+                        }
+                    });
+            });
+    });
+};
+
+module.exports = {
+    lastPublished,
+    getLastPublished,
+    getMangaState,
+    getScantradMangas,
+    getMangaDetails,
+};
